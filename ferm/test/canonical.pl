624e91d94123c720b9ac1d6fa73c0de6c59c5012
Max Kellermann <max@foo-projects.org>
2004-10-21 21:02:54 +0000

    implemented test suite
    git-svn-id: svn+ssh://foo-projects.org/var/svn/ferm/trunk@47 887c3e53-ddf1-0310-8e39-e0d3fab4ed34

---
+++ ferm/test/canonical.pl
@@ -0,0 +1,166 @@
+#!/usr/bin/perl -w
+# $Id$
+#
+# Canonicalize ferm output. You can use this script to check whether
+# the output from two ferm versions are functionally identical. This
+# is used in the compatibility tests.
+#
+# Author: Max Kellermann (max@duempel.org)
+
+use strict;
+
+my $data;
+
+while (<>) {
+    next if /^\s*(?:#.*)?$/s;
+
+    # workaround: not supported in ipchains
+    next
+      if /cannot set the policy for non-built in chains, exiting|Cannot create new chains if using ipfwadm|Ipfwadm allows only accept, masq, deny and reject targets/;
+
+    # execute backticks
+    s/`(.*?)`/`$1`/egs;
+
+    if (s/^iptables //) {
+        my $item;
+
+        # get table
+        my $table;
+        s/-t (\w+)/$table = $1; ''/eg;
+        $table = 'filter'
+          unless defined $table;
+
+        # get command and chain
+        my ($command, $chain);
+
+        if (s/-P (\w+) (\w+)//g) {
+            $data->{iptables}{$table}{$1}{policy} = $2;
+            next;
+        }
+
+        s/-([ALFZNXE])(?: (\w+))?/($command, $chain) = ($1, $2); ''/eg;
+
+        if ($command eq 'F' or $command eq 'N' or $command eq 'X') {
+            if (defined $chain) {
+                delete $data->{iptables}{$table}{$chain}{rules};
+            } else {
+                delete $data->{iptables}{$table};
+            }
+            next;
+        }
+
+        die 'no chain specified'
+          unless defined $chain;
+
+        # module list
+        my %modules;
+        s/-m (\w+)/$modules{$1} = 1; ''/eg;
+        $item->{modules} = [ keys %modules ];
+
+        # short to long
+        s/-j\b/--jump/g;
+        s/-i\b/--in-interface/g;
+        s/-o\b/--out-interface/g;
+        s/-p\b/--protocol/g;
+        s/-d\b/--destination/g;
+        s/-s\b/--source/g;
+
+        # evaluate options with zero, one, two parameters
+        s/(!\s*)?--(syn|clamp-mss-to-pmtu)\b/$item->{$2} = $1; ''/eg;
+        s/--(gid-owner|uid-owner|pid-owner|sid-owner|jump|protocol|
+             in-interface|out-interface|limit|mac-source|uid-owner|
+             tos|ttl-set|mark|state|
+             ulog-(?:nlgroup|prefix|cprange|qthreshold)|
+             psd-(?:weight-threshold|delay-threshold|lo-ports-weight|hi-ports-weight)|
+             source|destination|log-prefix|to|to-destination|dport|sport|
+             log-level|set-mss
+            )\s+(".*?"|(?:!\s*)?\S+)/$item->{$1} = $2; ''/egx;
+        s/--(tcp-flags)\s+(\S+)\s+(\S+)/$item->{$1} = [ $2, $3 ]; ''/eg;
+
+        # after we parsed everything we know, nothing must be left
+        die "unparsed rest from line $.: $_"
+          if /\S/;
+
+        # add this item
+        push @{$data->{iptables}{$table}{$chain}{rules}}, $item;
+    } elsif (s/^ipchains //) {
+        my $item;
+
+        # get command and chain
+        my ($command, $chain);
+
+        if (s/-P (\w+) (\w+)//g) {
+            $data->{ipchains}{$1}{policy} = $2;
+            next;
+        }
+
+        s/-([AFZNX])(?: (\w+))?/($command, $chain) = ($1, $2); ''/eg;
+
+        if ($command eq 'F' or $command eq 'N' or $command eq 'X') {
+            if (defined $chain) {
+                delete $data->{ipchains}{$chain}{rules};
+            } else {
+                delete $data->{ipchains};
+            }
+            next;
+        }
+
+        die 'no chain specified'
+          unless defined $chain;
+
+        # short to long
+        s/-j\b/--jump/g;
+        s/-i\b/--interface/g;
+        s/-d\b/--destination/g;
+        s/-s\b/--source/g;
+        s/-l\b/--log/g;
+        s/-p\b/--protocol/g;
+        s/-y\b/--syn/g;
+
+        # evaluate options with zero, one parameter
+        s/(!\s*)?--(log|syn)\b/$item->{$2} = $1; ''/eg;
+        s/--(jump|protocol|interface|destination|source|protocol|
+             dport|destination-port|sport|source-port
+            )\s+(".*?"|(?:!\s*)?\S+)/$item->{$1} = $2; ''/egx;
+
+        # after we parsed everything we know, nothing must be left
+        die "unparsed rest from line $.: $_"
+          if /\S/;
+
+        # add this item
+        push @{$data->{ipchains}{$chain}{rules}}, $item;
+    } elsif (s/^ipfwadm //) {
+        my $item;
+
+        # get chain
+        s/-([IOF])//
+          or die "No chain in line $.";
+
+        my $chain = $1;
+
+        # handle command
+        if (s/-p (\w+)//) {
+            $data->{ipfwadm}{$chain}{policy} = $1;
+            next;
+        }
+
+        $item->{policy} = $1
+          if s/-a (\w+)//;
+
+        # evaluate options
+        s/-([m])/$item->{$1} = 1; ''/eg;
+        s/-([PVW])\s+((?:!\s*)?\S+)/$item->{$1} = $2; ''/egx;
+
+        # after we parsed everything we know, nothing must be left
+        die "unparsed rest from line $.: $_"
+          if /\S/;
+
+        # add this item
+        push @{$data->{ipfwadm}{$chain}{rules}}, $item;
+    } else {
+        die "syntax error line $.";
+    }
+}
+
+use Data::Dumper;
+print Dumper($data);
