5bc1f3fd32969cdfcce3b7c6a40367583213b951
Max Kellermann <max@foo-projects.org>
2005-02-24 20:04:42 +0000

    ipt_dscp support
    git-svn-id: svn+ssh://foo-projects.org/var/svn/ferm/trunk@264 887c3e53-ddf1-0310-8e39-e0d3fab4ed34

--- ferm/test/canonical.pl
+++ ferm/test/canonical.pl
@@ -75,7 +75,7 @@ while (<>) {
 
         # evaluate options with zero, one, two parameters
         s/(!\s*)?--(syn|clamp-mss-to-pmtu|set|rcheck|log-tcp-sequence|log-tcp-options|log-ip-options|continue|save-mark|restore-mark|fragment)(\s|$)/$item->{$2} = $1; ''/eg;
-        s/--(tcp-flags)\s+(\S+)\s+(\S+)/$item->{$1} = [ $2, $3 ]; ''/eg;
+        s/--(tcp-flags|dscp-class)\s+(\S+)\s+(\S+)/$item->{$1} = [ $2, $3 ]; ''/eg;
         s/(!\s*)?--(iplimit-above)\s+(\S+)/$item->{$2} = [ $1, $2 ]; ''/eg;
         s/--(\w[-\w]*)\s+(".*?"|'.*?'|(?:!\s*)?\S+)/$item->{$1} = shell_unescape($2); ''/eg;
 
