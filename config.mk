42fd5ef1190d2017706e908c64564d91742eeb69
Max Kellermann <max@foo-projects.org>
2005-03-09 12:49:20 +0000

    fixed test suite; copy ferm1.1 from revision 21
    git-svn-id: svn+ssh://foo-projects.org/var/svn/ferm/trunk@367 887c3e53-ddf1-0310-8e39-e0d3fab4ed34

--- config.mk
+++ config.mk
@@ -4,3 +4,11 @@ DOCDIR = $(PREFIX)/share/doc/ferm
 
 WEBDIR = /home/web/users/koka/ferm
 FTPDIR = /home/tecguest/ises/koka/anonftp
+
+# location of the old ferm script, for the test suite
+OLD_FERM = $(TOPDIR)/test/ferm1.1
+OLD_OPTIONS = --use iptables --lines --noexec
+
+NEW_FERM = $(TOPDIR)/src/ferm
+NEW_OPTIONS = --test
+
