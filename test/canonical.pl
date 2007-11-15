609b3bffa71c4ae3abeb65d12ad570679c48d9f6
Max Kellermann <max@foo-projects.org>
2007-11-15 06:49:11 +0000

    support SECMARK, CONNSECMARK
    git-svn-id: svn+ssh://foo-projects.org/var/svn/ferm/branches/ferm-1.3.x@929 887c3e53-ddf1-0310-8e39-e0d3fab4ed34

--- test/canonical.pl
+++ test/canonical.pl
@@ -89,7 +89,7 @@ while (<>) {
           if exists $modules{set};
 
         # evaluate options with zero, one, two parameters
-        s/(?:(!)\s*)?--(syn|clamp-mss-to-pmtu|set|rcheck|log-tcp-sequence|log-tcp-options|log-ip-options|continue|save-mark|restore-mark|fragment|ecn-tcp-cwr|ecn-tcp-ece|physdev-is-(?:in|out|bridged)|strict|next|frag(res|first|more|last)|nodst|ecn-tcp-remove|ahres|soft|rt-0-res|rt-0-not-strict|ashort)(?:\s|$)/$item->{$2} = $1; ''/eg;
+        s/(?:(!)\s*)?--(syn|clamp-mss-to-pmtu|set|rcheck|log-tcp-sequence|log-tcp-options|log-ip-options|continue|save-mark|restore-mark|save|restore|fragment|ecn-tcp-cwr|ecn-tcp-ece|physdev-is-(?:in|out|bridged)|strict|next|frag(res|first|more|last)|nodst|ecn-tcp-remove|ahres|soft|rt-0-res|rt-0-not-strict|ashort)(?:\s|$)/$item->{$2} = $1; ''/eg;
         s/--(tcp-flags|chunk-types|add-set|del-set)\s+(?:(\!)\s+)?(\S+)\s+(\S+)/$item->{$1} = [ $2, $3, $4 ]; ''/eg;
         s/(?:(!)\s*)?--(iplimit-above|src-range|dst-range|connlimit-above|connbytes|tos)\s+(\S+)/$item->{$2} = [ $1, $2 ]; ''/eg;
         s/--(\w[-\w]*)\s+(!)?\s*(".*?"|'.*?'|\S+)/$item->{$1} = (defined $2 ? "$2\t" : "") . shell_unescape($3); ''/eg;
